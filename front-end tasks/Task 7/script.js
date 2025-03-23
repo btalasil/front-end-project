f365db57938147d516fbdca6d2c9bb278a028c2c
btalasil <btalasil@aum.edu>
2025-03-23 01:52:19 -0500

    Add files via upload

---
+++ front-end tasks/Task 7/script.js
@@ -0,0 +1,33 @@
+let x = 5;
+let y = 6;
+
+let add = x+y;
+let sub = x-y;
+let mul = x*y;
+let div = x/y;
+let exp = x**y;
+
+console.log(add)
+console.log(sub);
+console.log(mul)
+console.log(div)
+console.log(exp)
+
+
+let c=0
+c+=x;
+console.log(c)
+let d =0
+d-=y;
+console.log(d)
+
+console.log(x*=y)
+
+console.log(x/=y)
+console.log(x**=y)
+
+console.log(add)
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log(exp)
